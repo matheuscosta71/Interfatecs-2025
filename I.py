c4175f4554c1339b22c6edc0969304514fa19882
Matheus Costa <148267379+matheuscosta71@users.noreply.github.com>
2025-05-10 17:56:04 -0300

    I_Nonnavai

---
+++ I.py
@@ -0,0 +1,53 @@
+numero = int(input())
+
+for i in range (numero):
+    dorothy = int(input())
+    dagmar = int(input())
+
+    if dorothy > dagmar:
+        if dorothy + dagmar > 40:
+            print("DOROTHY DECIDE E A NONNA VAI")
+        else:
+            print("DOROTHY DECIDE")
+    else:
+        if dorothy + dagmar > 40:
+            print("DAGMAR DECIDE E A NONNA VAI")
+        else:
+            print("DAGMAR DECIDE")
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+    
+    
